1fc4ef3d9d6bdfe969630d4bd42cd7816463bb46
Dutzz23 <vlad.misaila@gmail.com>
2024-05-03 20:13:11 +0300

    Small clean up

--- app/models/ticket_model.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class Ticket(BaseModel):
-    start_date: str
-    end_date: str
-    category: str
--- app/views/router_animals_monitor.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-animals_monitor_router = APIRouter(prefix='/monitor', tags=['monitor'])
-
-
-@animals_monitor_router.get('/')
-def myLittleFunc():
-    return "Monitor router working!"
